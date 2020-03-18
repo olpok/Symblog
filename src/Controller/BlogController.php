4ea9b1114c66124d87dfaaf0d1f50486e653c251
olga <olgafff@free.fr>
2020-03-18 14:05:17 +0100

    Authentification user added

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Article;
 
+use App\Entity\Comment;
 use App\Form\ArticleType;
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -81,10 +83,25 @@ class BlogController extends AbstractController
      /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show(Article $article)
+    public function show(Article $article, Request $request, EntityManagerInterface $manager)
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setCreatedAt(new \DateTime())
+                    ->setArticle($article);
+            
+            $manager->persist($comment);
+            $manager->flush(); 
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
         return $this->render('blog/show.html.twig', [
-              'article' => $article
+              'article' => $article,
+              'commentForm' => $form->createView()
         ]);
     }
 
